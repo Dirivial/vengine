1392b6fe93542c117b8ada37782126662b3fcc0d
Dirivial <alexander.kadeby@gmail.com>
2026-01-18 23:06:17 +0100

    Something, not sure if it works

--- HelloTriangle/src/main.cpp
+++ HelloTriangle/src/main.cpp
@@ -1,45 +1,63 @@
+#include <typeinfo>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
-    void run() {
-		initWindow();
-		initVulkan();
-		mainLoop();
-		cleanup();
-    }
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
 
 private:
-    void initWindow() {
-    
-    }
-    void initVulkan() {
-
+  void initWindow() {
+    if (!glfwInit()) {
+      throw std::runtime_error("GLFW initialization failed");
     }
 
-    void mainLoop() {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan() {}
+
+  void mainLoop() {
+    std::cout << "Hello triangle!" << std::endl;
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
     }
+    std::cout << "Bye triangle!" << std::endl;
+  }
 
-    void cleanup() {
+  void cleanup() {
+    glfwDestroyWindow(window);
 
-    }
+    glfwTerminate();
+  }
+
+private:
+  GLFWwindow *window;
 };
 
 int main() {
-    HelloTriangleApplication app;
+  HelloTriangleApplication app;
 
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
