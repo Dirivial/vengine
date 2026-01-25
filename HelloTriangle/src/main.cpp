6c0b78f7c19c2a0961ebb4c9b4839e32ede86466
Dirivial <alexander.kadeby@gmail.com>
2026-01-25 22:13:25 +0100

    Apparently nothing is shown until I draw...

--- HelloTriangle/src/main.cpp
+++ HelloTriangle/src/main.cpp
@@ -1,63 +1,60 @@
-#include <typeinfo>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <cstdlib>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
-  void run() {
-    initWindow();
-    initVulkan();
-    mainLoop();
-    cleanup();
-  }
-
-private:
-  void initWindow() {
-    if (!glfwInit()) {
-      throw std::runtime_error("GLFW initialization failed");
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-  }
-  void initVulkan() {}
-
-  void mainLoop() {
-    std::cout << "Hello triangle!" << std::endl;
-    while (!glfwWindowShouldClose(window)) {
-      glfwPollEvents();
-    }
-    std::cout << "Bye triangle!" << std::endl;
-  }
-
-  void cleanup() {
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-  }
-
 private:
-  GLFWwindow *window;
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 };
 
 int main() {
-  HelloTriangleApplication app;
+    HelloTriangleApplication app;
 
-  try {
-    app.run();
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-  }
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
